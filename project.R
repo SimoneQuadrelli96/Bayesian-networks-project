43d76388b9f4b5675b71ef13a3bb155500739135
Simone Quadrelli <alalai91@hotmail.it>
2020-03-12 11:37:06 +0100

    log-linear model

--- project.R
+++ project.R
@@ -18,41 +18,45 @@ data$exang <- factor(data$exang)
 data$slope <- factor(data$slope)
 data$ca <- factor(data$ca)
 data$thal <- factor(data$thal)
+data$chol[data$chol <= 240 ]  <- 0
 data$chol[data$chol > 240]  <- 1
-data$chol[data$chol <= 240]  <- 0
 names(data)[names(data) == "chol"] <- "hyperchol"
 data$hyperchol <- factor(data$hyperchol)
+data$class_age <- data$age
+head(data)
+data$class_age[data$class_age<45] <- 1
+data$class_age[ data$class_age>=45 & data$class_age<=65] <- 2
+data$class_age[data$class_age>65] <- 3
 head(data)
 
+
+# Analysis of the distributions of discrete  variables
+barplot(prop.table(table(data$target)), main="Disease",names.arg=c("No", "Yes") )
+barplot(prop.table(table(data$sex)), main="Sex distribution",  names.arg=c("Female", "Male"))
+barplot(prop.table(table(data$cp)), main="Chest pain distribution",  names.arg=c("No", "Yes"))
+barplot(prop.table(table(data$fbs)), main="Fasting blood sugar > 120 mg/dl distribution",  names.arg=c("No", "Yes"))
+barplot(prop.table(table(data$restecg)), main="Resting electrocardiographic result distribution",  names.arg=c("normal", "ST-T ab.","hypertrophy"))
+barplot(prop.table(table(data$slope)), main="Slope of the peak exercise ST segment distribution",  names.arg=c("upsloping", "flat","downsloping"))
+barplot(prop.table(table(data$ca)), main="Major vessels colored by flourosopy distribution")
+barplot(prop.table(table(data$thal)), main="Thal distribution")
+barplot(prop.table(table(data$hyperchol)), main="Hypercholestrol distribution",  names.arg=c("No", "Yes"))
+barplot(prop.table(table(data$class_age)), main="Age class distribution", names.arg=c("< 45", ">= 45 & <= 65", "> 65") )
+
 # create table
-data_table <- as.table(ftable(data[c(1,3,4,6,7,8,10,12,13)]))
-data_table
+data_table <- as.table(ftable(data[c(1,3,4,6,7,8,10,12,13,15)]))
 
-#models
-m.init <- dmod(~.^.,data=data_table) # saturated model
-x11()
-plot(m.init)
-
-# default  values:
-# penalty parameter = 2 (AIC)
-# direction: backward
-m1 <- stepwise(m.init)
-x11()
-plot(m1)
-
-# parameter of penalization is log(N)
-m2 <- stepwise(m.init,k=log(sum(data_table)))
-x11()
-plot(m2)
-
+#model selection
+# saturated model
+m_sat <- dmod(~.^.,data=data_table) 
+m1 <- stepwise(m_sat)
+m2 <- stepwise(m_sat,k=log(sum(data_table)))
+# start from the null model
+m3  <- stepwise(m_sat,k=log(sum(data_table)),direction="forward",details=1)
 
 
 x11()
-par(mfrow=c(1,3))
-plot(as(m.init,"igraph"),main="Initial model")
+par(mfrow=c(2,2))
+plot(as(m.init,"igraph"),main="Initial model backward")
+plot(as(m3,"igraph"),main="Initial model forward")
 plot(as(m1,"igraph"), main="AIC")
 plot(as(m2,"igraph"), main="BIC")
-
-m3  <- stepwise(m.init,k=log(sum(data_table)),direction="forward",details=1)
-x11()
-plot(m3)
